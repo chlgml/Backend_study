03cce0e213507aaa086696eeec91065ebcacfc70
최희 <cmlgml094356@gmail.com>
2022-01-16 23:09:15 +0900

    Move the folder

---
+++ spring boot/security/token/JWTcode_study/src/main/java/com/example/jwtcode_study/JwTcodeStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtcode_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwTcodeStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwTcodeStudyApplication.class, args);
+    }
+
+}
