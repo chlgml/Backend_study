cbde622902843ddb29ca4ed86736ea48d6c1310e
최희 <cmlgml094356@gmail.com>
2022-03-07 10:26:07 +0900

    [DELETE] CRUD코드 삭제

--- spring boot/crud_code/crud (1)/src/main/java/com/example/project/Exception/ConflictException.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.Exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.CONFLICT) // 409
-public class ConflictException  extends RuntimeException{
-}
